c695ef6947e07007b563dec4e750c4831238d435
julianmartire1 <julianmartire.jm@gmail.com>
2016-11-16 17:11:41 -0300

    Trabajo Practico 2(Recuperatorio)

---
+++ RecuperatoriosTP/Trabajo Practico 2/Alumno_Clase_12/Clase_12_Library/Camion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library
+{
+    public class Camion : Vehiculo
+    {
+
+        #region Constructor
+
+        public Camion(EMarca marca, string patente, ConsoleColor color)
+            : base(marca, patente, color)
+        {
+        }
+
+        #endregion
+
+        #region Propiedad
+        /// <summary>
+        /// Los camiones tienen 8 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 8;
+            }
+            set
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region Metodo
+        /// <summary>
+        /// Muestra los datos del Camion
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMION");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS : " + this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ RecuperatoriosTP/Trabajo Practico 2/Alumno_Clase_12/Clase_12_Library/Moto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_12_Library
+{
+    public class Moto : Vehiculo
+    {
+        #region Constructor
+
+        public Moto(EMarca marca, string patente, ConsoleColor color)
+            : base(marca, patente, color)
+        {
+        }
+
+        #endregion
+
+        #region Propiedades
+        /// <summary>
+        /// Las motos tienen 2 ruedas
+        /// </summary>        
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 2;
+            }
+            set
+            {
+
+            }
+        }
+        #endregion
+
+        #region Metodo
+        /// <summary>
+        /// Muestra los datos de la Moto
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS : " + this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
